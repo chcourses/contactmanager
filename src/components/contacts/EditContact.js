8ab1d8610d2bbe4e374c69f7f66468e4fe1f6bbf
Carlos Henrique <chcourses@gmail.com>
2018-09-08 20:42:15 -0300

    initial commit

---
+++ src/components/contacts/EditContact.js
@@ -0,0 +1,136 @@
+import React, { Component } from 'react';
+import { Consumer } from '../../context';
+import TextInputGroup from '../layout/TextInputGroup';
+//import uuid from 'uuid';
+import axios from 'axios';
+
+class EditContact extends Component {
+
+  state = {
+    name: '',
+    email:'',
+    phone: '',
+    errors: {}
+  }
+
+  async componentDidMount() {
+    const { id } = this.props.match.params;
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+    this.setState(
+      {
+        name: response.data['name'],
+        email: response.data['email'],
+        phone: response.data['phone']
+      }
+    );
+  }
+
+  onChangeHandler = e => this.setState(
+    { [e.target.name]: e.target.value }
+  );
+
+  onSubmit = async (dispatch, e) => {
+    e.preventDefault();
+    const { name, email, phone } = this.state;
+
+    // CHeck For Errors
+    if (name === '') {
+      this.setState({
+        errors: { name: 'Name is required'}
+      });
+      return;
+    }
+
+    if (email === '') {
+      this.setState({
+        errors: { email: 'Email is required'}
+      });
+      return;
+    }
+
+    if (phone === '') {
+      this.setState({
+        errors: { phone: 'Phone is required'}
+      });
+      return;
+    }
+
+    const updContact = {
+      
+      name,
+      email,
+      phone,
+
+    }
+
+    const { id } = this.props.match.params;
+
+    const res = await axios
+      .put(`https://jsonplaceholder.typicode.com/users/${id}`, updContact);
+
+    dispatch({type: 'UPDATE_CONTACT', payload: res.data});
+    
+    // CLEAR STATE
+    this.setState({
+      name: '',
+      email: '',
+      phone: '',
+      errors: {}
+    });
+
+    this.props.history.push('/');
+  }
+
+  render() {
+    const { name, email, phone, errors } = this.state;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card mb-3">
+              <div className="card-header">Edit contact</div>
+                <div className="card-body">
+                  <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                    <TextInputGroup 
+                      label="Name"
+                      name="name"
+                      placeholder="Enter name"
+                      value={name}
+                      onChange={this.onChangeHandler}
+                      error={errors.name}
+                    />
+                    <TextInputGroup 
+                      type="email"
+                      label="Email"
+                      name="email"
+                      placeholder="Enter email"
+                      value={email}
+                      onChange={this.onChangeHandler}
+                      error={errors.email}
+                    />
+                    <TextInputGroup 
+                      label="Phone"
+                      name="phone"
+                      placeholder="Enter phone"
+                      value={phone}
+                      onChange={this.onChangeHandler}
+                      error={errors.phone}
+                    />
+                    <input
+                      type="submit" 
+                      value="Update Contact" 
+                      className="btn btn-light btn-block"
+                    />
+                  </form>
+              </div>
+            </div>
+          )
+        }}
+      </Consumer>
+    );
+  }
+}
+
+export default EditContact;
